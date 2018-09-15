e515f7422851999473983a597b5a4778e9a428bf
Mor Merhav <mormerhav@gmail.com>
2018-09-15 21:47:04 +0300

    Externalize name2Exec.json

--- src/main/java/com/mmerhav/remotecontrolserver/manager/ExecutablesManagerImpl.java
+++ src/main/java/com/mmerhav/remotecontrolserver/manager/ExecutablesManagerImpl.java
@@ -3,10 +3,12 @@ package com.mmerhav.remotecontrolserver.manager;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.io.IOUtils;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
@@ -16,12 +18,15 @@ import java.util.Map;
 @Component
 public class ExecutablesManagerImpl implements ExecutablesManager {
 
+    @Value("${path.to.exec.file}")
+    private String pathToExecFile;
+
     private Map<String,String> name2ExecMap;
 
     @PostConstruct
     public void init() throws IOException {
-        ClassPathResource classPathResource = new ClassPathResource("name2Exec.json");
-        try (InputStream inputStream = classPathResource.getInputStream()) {
+        File execFile = new File(pathToExecFile);
+        try (InputStream inputStream = new FileInputStream(execFile)) {
             String data = IOUtils.toString(inputStream, Charset.forName("UTF8"));
             Type type = new TypeToken<Map<String, String>>(){}.getType();
             name2ExecMap = new Gson().fromJson(data, type);
